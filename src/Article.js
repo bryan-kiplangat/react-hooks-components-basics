f707d7f5e89659fb91308b24331629209acb712b
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-10-25 15:41:58 +0300

    Completed assignment

--- src/Article.js
+++ src/Article.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+// we declare a function, Article
 function Article() {
+  // we explicitly provide a return value of JSX (we tell React "Hey, when you want to put this component on the DOM, here is what it should become!")
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    <div>Dear Reader: Bryan Bett has the best code</div>
   );
 }
 
